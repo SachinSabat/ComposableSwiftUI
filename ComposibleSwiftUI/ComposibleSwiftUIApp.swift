5639e2f91173e7cef4c2f91cac3bd5881075c1d9
Sachin Sabat <sachin.sabat@publicissapient.com>
2024-01-20 18:03:09 +0530

    Initial Setup

---
+++ ComposibleSwiftUI/ComposibleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposibleSwiftUIApp.swift
+//  ComposibleSwiftUI
+//
+//  Created by Sachin Sabat on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ComposibleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
